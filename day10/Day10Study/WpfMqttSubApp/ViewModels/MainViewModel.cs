ae6c2e8ef45fa3c64b7e0a35e64551c1069992b4
chs491 <hyunsoo1702@gmail.com>
2025-05-21 13:55:33 +0900

    10일차 커밋

---
+++ day10/Day10Study/WpfMqttSubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMqttSubApp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        private readonly IDialogCoordinator dialogCoordinator;
+        private string _brokerHost;
+        private string _databaseHost;
+
+        // 속성 BrokerHost, DatabaseHost
+        // 메서드 ConnectBrokerCommand, ConnectDatabaseCommand
+        public MainViewModel(IDialogCoordinator coordinator) 
+        {
+            this.dialogCoordinator = coordinator;
+
+            // BrokerHost = "211.119.12.75";
+            BrokerHost = "210.119.12.52"; // 강사 PC로 접속
+            DatabaseHost = "211.119.12.75"; // 본인 PC아이피 MySQL 서버 그대로
+        }
+
+        public string BrokerHost
+        {
+            get => _brokerHost;
+            set => SetProperty(ref _brokerHost, value); 
+        }
+
+        public string DatabaseHost
+        {
+            get => _databaseHost;
+            set => SetProperty(ref _databaseHost, value);   
+        }
+
+        [RelayCommand]
+        public async Task ConnectBroker()
+        {
+            await this.dialogCoordinator.ShowMessageAsync(this, "브로커 연결", "브로커연결합니다!");
+        }
+
+        [RelayCommand]
+        public async Task ConnectDatabase()
+        {
+            await this.dialogCoordinator.ShowMessageAsync(this, "DB 연결", "DB연결합니다!");
+        }
+    }
+}
